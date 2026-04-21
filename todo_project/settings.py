5d6d1f4767d65fdc18971263f0bc16142d32e0d4
Sushil Kumar Nayak <sushil.nayak98@yahoo.com>
2026-04-21 19:19:24 +0530

    Update TEMPLATES configuration in settings.py
    Replaced TEMPLATES configuration with a complete structure including context processors.

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -3,4 +3,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todo',
 ]
-TEMPLATES[0]['APP_DIRS'] = True  
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],  # you can add custom template dirs here later
+        'APP_DIRS': True,  # IMPORTANT for app templates
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+] 
