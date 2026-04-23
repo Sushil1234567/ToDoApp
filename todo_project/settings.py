1ccd16aec45b81752580e3db7ffda5f923498d84
Sushil Kumar Nayak <sushil.nayak98@yahoo.com>
2026-04-23 19:23:33 +0530

    Switched database from PostgreSQL to SQLite
    Changed database configuration from PostgreSQL to SQLite for local development.

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -1,4 +1,4 @@
-import os    #requiredforDB
+import os    #requiredforSecurity
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,12 +41,8 @@ TEMPLATES = [
 # DATABASE 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'dummy.sqlite3',
     }
 }
 
